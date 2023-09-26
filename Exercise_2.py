bc7facf3dd469d8bae8bafb374fb69f096f1316e
Avinoam Makdoosi <avinoam.mak@gmail.com>
2023-09-26 14:30:14 +0300

    my exercise 2 solution

--- Exercise_2.py
+++ Exercise_2.py
@@ -4,8 +4,40 @@ def morse_file_translator(file_name):
         morse = f.read()
         english = ""
         words = []
-
+        max_letter = ""
+        max_value = 0
         words = morse.split(" ")
+
+        english_copy = {}
+        english_letter_dictionary = {
+            'A': 0,
+            'B': 0,
+            'C': 0,
+            'D': 0,
+            'E': 0,
+            'F': 0,
+            'G': 0,
+            'H': 0,
+            'I': 0,
+            'J': 0,
+            'K': 0,
+            'L': 0,
+            'M': 0,
+            'N': 0,
+            'O': 0,
+            'P': 0,
+            'Q': 0,
+            'R': 0,
+            'S': 0,
+            'T': 0,
+            'U': 0,
+            'V': 0,
+            'W': 0,
+            'X': 0,
+            'Y': 0,
+            'Z': 0,
+            " ": 0
+        }
         morse_dictionary = {
             ".-": "A",
             "-...": "B",
@@ -43,19 +75,31 @@ def morse_file_translator(file_name):
             "--...": "7",
             "---..": "8",
             "----.": "9",
-            "/": " ",  # Space between words
+            "/": " "
         }
 
         for i in words:
-            english += morse_dictionary.get(i, "!")
+            translation = morse_dictionary.get(i, "!")
+            english += translation
+            english_letter_dictionary[translation] += 1
 
-        for i in english:
-            if (i == "!"):
-                print("Error in Morse Code")
-                break
-        else:
-            print()
+        print(english)
         f.close()
+
+        while(max(english_letter_dictionary.values()) > 0):
+            max_value = max(english_letter_dictionary.values())
+            for key, value in english_letter_dictionary.items():
+                if (value == max_value):
+                    max_letter += key
+                    english_letter_dictionary.update({key: 0})
+            print(f'{max_letter}: {max_value}')
+            max_letter = ""
 
     except FileNotFoundError:
         print("error in file name")
+
+
+
+
+
+morse_file_translator("morse.txt")
